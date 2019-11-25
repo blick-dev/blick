bea390aeca5228829997b4d2b240aacb5565b913
Marc Stammerjohann <marcstammerjohann@web.de>
2019-11-26 00:41:36 +0100

    feat(detach-device): disable resizable and fullscreen

--- electron/main-process/detach-device.js
+++ electron/main-process/detach-device.js
@@ -3,7 +3,9 @@ const { ipcMain, BrowserWindow } = require('electron');
 ipcMain.on('detach-device', (event, arg) => {
   let window = new BrowserWindow({
     height: arg.device.height,
-    width: arg.device.width
+    width: arg.device.width,
+    resizable: false,
+    fullscreen: false
   });
 
   window.on('close', () => (window = null));
