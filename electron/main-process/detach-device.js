5adbd91da097e800017ec87f198dc904a9d1fe71
Marc Stammerjohann <marcstammerjohann@web.de>
2019-11-26 21:25:04 +0100

    feat(detach-device): load url in browserview
    * add a toolbar to the detached device browser window

--- electron/main-process/detach-device.js
+++ electron/main-process/detach-device.js
@@ -1,17 +1,36 @@
-const { ipcMain, BrowserWindow } = require('electron');
+const { ipcMain, BrowserWindow, BrowserView } = require('electron');
+
+const toolbarHeight = 56;
 
 ipcMain.on('detach-device', (event, arg) => {
   const isPortraitMode = arg.device.orientation === 'portrait';
+
+  const height = isPortraitMode
+    ? arg.device.height + toolbarHeight
+    : arg.device.width + toolbarHeight;
+  const width = isPortraitMode ? arg.device.width : arg.device.height;
+
   let window = new BrowserWindow({
-    height: isPortraitMode ? arg.device.height : arg.device.width,
-    width: isPortraitMode ? arg.device.width : arg.device.height,
+    height: height,
+    width: width,
     resizable: false,
     fullscreen: false
   });
 
   window.on('close', () => (window = null));
-  window.loadURL(arg.url);
+  window.loadURL(`file://${__dirname}/../src/detach-device.html`);
   window.show();
+
+  let view = new BrowserView();
+  window.setBrowserView(view);
+  view.setBounds({
+    x: 0,
+    y: toolbarHeight,
+    height: height,
+    width: width
+  });
+  // TODO add user agent to load url
+  view.webContents.loadURL(arg.url);
 
   event.sender.send('detach-device-reply', { windowId: window.id });
 });
